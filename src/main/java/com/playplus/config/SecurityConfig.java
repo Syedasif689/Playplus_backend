680f700421e9c1ade66c7409cdd58c285735703d
Syedasif689 <asifsayed635@gmail.com>
2026-07-09 10:47:59 +0530

    updated frontend link

--- src/main/java/com/playplus/config/SecurityConfig.java
+++ src/main/java/com/playplus/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public CorsConfigurationSource corsConfigurationSource() {
     "http://localhost:3000",
     "https://playplus-frontend.vercel.app",
     "https://play-plus-enter.vercel.app",
-    "https://playplus-frontend-six.vercel.app"   // ✅ New frontend
+    "https://playplus-on.vercel.app/"   // ✅ New frontend
 ));
     configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
     configuration.setAllowedHeaders(Arrays.asList("*"));
